5b4a9b52be0c1985836a670a78f2805353e0c170
saif96 <contact.techosaif@gmail.com>
2019-11-05 01:22:05 +0530

    implemented spinner functionality

--- src/base/interfaces/IAppGlobalProps.ts
+++ src/base/interfaces/IAppGlobalProps.ts
@@ -1,6 +1,7 @@
 import AppGlobalState from "../../redux/states/AppGlobalState";
 
 export default interface IAppGlobalProps {
-    appGlobalState: AppGlobalState,
-    navigation: any
+    appGlobalState?: AppGlobalState;
+    children?: React.ReactNode;
+    navigation?: any;
 }
--- src/components/general-components/SpinAndWinComponent.tsx
+++ src/components/general-components/SpinAndWinComponent.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { View, Button, Text } from "native-base";
-import Svg, { Circle, Path, G, Rect, Polygon } from "react-native-svg";
+import Svg, { Circle, Path, G, Polygon, TextPath, Text as SvgText, TSpan, Defs } from "react-native-svg";
 import ColorConstants from "../../core/constants/ColorConstants";
 import { Dimensions } from "react-native";
+import IAppGlobalProps from "../../base/interfaces/IAppGlobalProps";
+import GlobalStyle from "../../styles/GlobalStyle";
 
-
-interface IProps {
+interface IProps extends IAppGlobalProps {
 
 }
 type Props = IProps
@@ -32,13 +33,13 @@ class SpinAndWinComponent extends React.Component<Props, IState> {
     startSpinner = () => {
         let thisComponent = this;
         let intervalId = setInterval(function () {
-            thisComponent.setState({ rotationDegree: thisComponent.state.rotationDegree + 30 });
+            thisComponent.setState({ ...thisComponent.state, rotationDegree: thisComponent.state.rotationDegree + 60 });
         }, 0);
 
         setTimeout(function () {
             clearInterval(intervalId);
-            thisComponent.setState({ rotationDegree: (6 * 60 - 30) });
-        }, 5000);
+            thisComponent.setState({ rotationDegree: (5 * 60 - 30) });
+        }, 1000);
     }
 
     polarToCartesian(centerX: number, centerY: number, radius: number, angleInDegrees: number) {
@@ -67,9 +68,21 @@ class SpinAndWinComponent extends React.Component<Props, IState> {
         return d;
     }
 
-    getRotationDegree = () => {
+    describeLine(cx: number, cy: number, rad: number, cornerGrad: number) {
+        let cornerRad = cornerGrad * Math.PI / 180;
+        let nx = Math.cos(cornerRad) * rad + cx;
+        let ny = Math.sin(cornerRad) * rad + cy;
+        let points = { x: nx, y: ny };
 
+        let d = [
+            "M", cx, cy,
+            "L", points.x, points.y,
+            "Z"
+        ].join(" ");
+
+        return d;
     }
+
 
     render() {
 
@@ -93,11 +106,6 @@ class SpinAndWinComponent extends React.Component<Props, IState> {
                     }}
                 >
 
-
-                    <Button onPress={() => this.startSpinner()}>
-                        <Text>Animate</Text>
-                    </Button>
-
                     <Svg
                         height={this.state.svgHeight}
                         width={this.state.svgWidth}
@@ -105,15 +113,32 @@ class SpinAndWinComponent extends React.Component<Props, IState> {
                         fill="none"
                     >
 
-
                         <G transform={`rotate(${this.state.rotationDegree}, ${cOriginXAxis} ${cOriginYAxis})`}>
 
-                            <Path d={this.describeArc(cOriginXAxis, cOriginYAxis, c1Radius, 0, 60)} fill="#de3b18" />
-                            <Path d={this.describeArc(cOriginXAxis, cOriginYAxis, c1Radius, 60, 120)} fill="#706eaf" />
-                            <Path d={this.describeArc(cOriginXAxis, cOriginYAxis, c1Radius, 120, 180)} fill="#3ba6d2" />
-                            <Path d={this.describeArc(cOriginXAxis, cOriginYAxis, c1Radius, 180, 240)} fill="#51b5a9" />
-                            <Path d={this.describeArc(cOriginXAxis, cOriginYAxis, c1Radius, 240, 300)} fill="#dfa930" />
-                            <Path d={this.describeArc(cOriginXAxis, cOriginYAxis, c1Radius, 300, 360)} fill="#e8404f" />
+                            <Path
+                                id="cSector1"
+                                d={this.describeArc(cOriginXAxis, cOriginYAxis, c1Radius, 0, 60)} fill="#de3b18"
+                            />
+                            <Path
+                                id="cSector2"
+                                d={this.describeArc(cOriginXAxis, cOriginYAxis, c1Radius, 60, 120)} fill="#706eaf"
+                            />
+                            <Path
+                                id="cSector3"
+                                d={this.describeArc(cOriginXAxis, cOriginYAxis, c1Radius, 120, 180)} fill="#3ba6d2"
+                            />
+                            <Path
+                                id="cSector4"
+                                d={this.describeArc(cOriginXAxis, cOriginYAxis, c1Radius, 180, 240)} fill="#51b5a9"
+                            />
+                            <Path
+                                id="cSector5"
+                                d={this.describeArc(cOriginXAxis, cOriginYAxis, c1Radius, 240, 300)} fill="#dfa930"
+                            />
+                            <Path
+                                id="cSector6"
+                                d={this.describeArc(cOriginXAxis, cOriginYAxis, c1Radius, 300, 360)} fill="#e8404f"
+                            />
 
                             <Circle
                                 id="circleWrapper"
@@ -125,7 +150,130 @@ class SpinAndWinComponent extends React.Component<Props, IState> {
                                 fill="none"
                             />
 
+                            <G id="sectorTitles">
+                                <SvgTitleTextWrapper>
+                                    <TextPath
+                                        href="#cSector1"
+                                    >
+                                        Sorry
+                                 </TextPath>
+                                </SvgTitleTextWrapper>
 
+                                <SvgTitleTextWrapper>
+                                    <TextPath
+                                        href="#cSector2"
+                                    >
+                                        Win
+                                </TextPath>
+                                </SvgTitleTextWrapper>
+
+                                <SvgTitleTextWrapper>
+                                    <TextPath
+                                        href="#cSector3"
+                                    >
+                                        Chance to win
+                                 </TextPath>
+                                </SvgTitleTextWrapper>
+
+                                <SvgTitleTextWrapper>
+                                    <TextPath
+                                        href="#cSector4"
+                                    >
+                                        Win
+                                 </TextPath>
+                                </SvgTitleTextWrapper>
+
+                                <SvgTitleTextWrapper>
+                                    <TextPath
+                                        href="#cSector5"
+                                    >
+                                        Chance to win
+                                 </TextPath>
+                                </SvgTitleTextWrapper>
+
+                                <SvgTitleTextWrapper>
+                                    <TextPath
+                                        href="#cSector6"
+                                    >
+                                        Chance to win
+                                </TextPath>
+                                </SvgTitleTextWrapper>
+                            </G>
+
+                            <G id="sectorDescription">
+                                <Defs>
+                                    <Path
+                                        d={this.describeLine(cOriginXAxis, cOriginYAxis, c1Radius, 300)}
+                                        id="cSector1Des"
+                                    />
+                                    <Path
+                                        d={this.describeLine(cOriginXAxis, cOriginYAxis, c1Radius, 360)}
+                                        id="cSector2Des"
+                                    />
+                                    <Path
+                                        d={this.describeLine(cOriginXAxis, cOriginYAxis, c1Radius, 60)}
+                                        id="cSector3Des"
+                                    />
+                                    <Path
+                                        d={this.describeLine(cOriginXAxis, cOriginYAxis, c1Radius, 120)}
+                                        id="cSector4Des"
+                                    />
+
+                                    <Path
+                                        d={this.describeLine(cOriginXAxis, cOriginYAxis, c1Radius, 180)}
+                                        id="cSector5Des"
+                                    />
+
+                                    <Path
+                                        d={this.describeLine(cOriginXAxis, cOriginYAxis, c1Radius, 240)}
+                                        id="cSector6Des"
+                                    />
+                                </Defs>
+
+                                <SvgDesTextWrapper>
+                                    <TextPath href="#cSector1Des">
+                                        <TSpan dy="-3%">
+                                            Better
+                                        </TSpan>
+                                        <TSpan dy="5%" dx="-12%">
+                                            luck next
+                                        </TSpan>
+                                        <TSpan dy="5%" dx="-17%">
+                                            time
+                                        </TSpan>
+                                    </TextPath>
+                                </SvgDesTextWrapper>
+
+                                <SvgDesTextWrapper>
+                                    <TextPath href="#cSector2Des">
+                                        Rs.100
+                                    </TextPath>
+                                </SvgDesTextWrapper>
+
+                                <SvgDesTextWrapper>
+                                    <TextPath href="#cSector3Des">
+                                        Rs.21,00
+                                    </TextPath>
+                                </SvgDesTextWrapper>
+
+                                <SvgDesTextWrapper>
+                                    <TextPath href="#cSector4Des">
+                                        Rs.11,000
+                                    </TextPath>
+                                </SvgDesTextWrapper>
+
+                                <SvgDesTextWrapper>
+                                    <TextPath href="#cSector5Des">
+                                        Rs.1,000
+                                    </TextPath>
+                                </SvgDesTextWrapper>
+
+                                <SvgDesTextWrapper>
+                                    <TextPath href="#cSector6Des">
+                                        Rs.1800
+                                    </TextPath>
+                                </SvgDesTextWrapper>
+                            </G>
                             <Circle
                                 id="circleInnerWrapper"
                                 cx={cOriginXAxis}
@@ -135,35 +283,43 @@ class SpinAndWinComponent extends React.Component<Props, IState> {
                                 stroke={ColorConstants.white}
                                 fill="none"
                             />
+
                         </G>
 
-                        <Polygon
-                            points={`
+                        <G id="indicator">
+                            <Polygon
+                                points={`
                                         ${cOriginXAxis - c3Radius},${cOriginYAxis} 
                                         ${cOriginXAxis}, ${cOriginYAxis - c3Radius * 3} 
                                         ${cOriginXAxis + c3Radius}, ${cOriginYAxis}
                                         `}
-                            strokeWidth="2"
-                            fill={ColorConstants.lightGray2}
-                        />
+                                strokeWidth="2"
+                                fill={ColorConstants.lightGray2}
+                            />
 
-                        <Circle
-                            id="circlePointer"
-                            cx={cOriginXAxis}
-                            cy={cOriginYAxis}
-                            r={c3Radius}
-                            strokeWidth="2"
-                            stroke={ColorConstants.white}
-                            fill="#7f7b7f"
-                        />
-
+                            <Circle
+                                id="circlePointer"
+                                cx={cOriginXAxis}
+                                cy={cOriginYAxis}
+                                r={c3Radius}
+                                strokeWidth="2"
+                                stroke={ColorConstants.white}
+                                fill="#7f7b7f"
+                            />
+                        </G>
 
                     </Svg>
 
                 </View>
-                <View
-                    style={{ backgroundColor: "red", flex: 1, flexDirection: "column" }}
-                >
+                <View style={GlobalStyle.verticalSpacing}>
+                    <Button
+                        full
+                        primary
+                        style={GlobalStyle.borderRadiusM}
+                        onPress={() => this.startSpinner()}
+                    >
+                        <Text>Tap to spin</Text>
+                    </Button>
                 </View>
             </View >
 
@@ -172,3 +328,40 @@ class SpinAndWinComponent extends React.Component<Props, IState> {
 }
 
 export default SpinAndWinComponent;
+
+
+
+const SvgTitleTextWrapper = (props: IAppGlobalProps) => {
+    return (
+        <SvgText
+            fontStretch="extra-condensed"
+            letterSpacing={10}
+            fontSize="16px"
+            fontWeight="800"
+            fill={ColorConstants.white}
+            dx="25%"
+            dy="-4%"
+            textLength="100%"
+            textAnchor="middle"
+        >
+            {props.children}
+        </SvgText>
+    )
+}
+
+const SvgDesTextWrapper = (props: IAppGlobalProps) => {
+    return (
+        <SvgText
+            fontStretch="extra-condensed"
+            fontSize="16px"
+            fontWeight="800"
+            dx="22%"
+            dy="2%"
+            fill={ColorConstants.white}
+            textLength="100%"
+            textAnchor="middle"
+        >
+            {props.children}
+        </SvgText>
+    )
+}
